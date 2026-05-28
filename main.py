93714360daef8bbfda5d641c6f440e183155ad7f
Nizigiymana-Tech <junkacc830@gmail.com>
2026-05-28 11:19:44 -0400

    lets goo

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from tkinter import *
+from tkinter import *  # noqa: F403
 from tkinter import ttk
 from tkinter.font import Font
 
-from Words import *
+from Words import *  # noqa: F403
 
 WhitelistedKeys = ["\r", "\x08"]
+
+letterID = 0
 
 MaxRow = 5
 MaxButtons = 5
@@ -14,7 +16,7 @@ labels = []
 Word = RNGWord()
 PWord = []
 
-root = Tk()
+root = Tk()  # noqa: F405
 style = ttk.Style()
 
 root.title("Wordle")
@@ -23,7 +25,7 @@ root.configure(background="black")
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
 
-def OnTyped(Key: StringVar):
+def OnTyped(Key):
     global letters
     global labels
 
@@ -31,14 +33,9 @@ def OnTyped(Key: StringVar):
         if not Key.char in WhitelistedKeys:
             return
 
-    print(Key)
-    print(labels)
     if Key.char == "\r":
         if len(letters) == 5:
-            print(labels)
             for label, wordCharacter, number in zip(labels, Word, range(len(labels))):
-                print(label)
-                print(wordCharacter)
                 if label['text'] == wordCharacter:
                     label.configure(background="Green", highlightbackground="Green")
 
@@ -48,7 +45,6 @@ def OnTyped(Key: StringVar):
                     else:
                         PWord.insert(int(number), str(wordCharacter))
 
-            print(PWord)
             letters = []
 
             for i in range(1, 6):
@@ -64,7 +60,7 @@ def OnTyped(Key: StringVar):
             return
         
         if Key.char == "\x08":
-            for label in reversed(labels):
+            for label in reversed(letters):
                 if label['text'] != "":
                     letters.pop()
                     label.configure(text="")
@@ -77,19 +73,19 @@ def OnTyped(Key: StringVar):
             if index['text']:
                 continue
             else:
-                print(index)
                 index.configure(text=str(Key.char.capitalize()))
-                letters.append(str(Key.char.capitalize()))
+                letters.append(index)
+                print(letters)
                 break
 
-mainframe = Frame(root, background="black")
+mainframe = Frame(root, background="black")  # noqa: F405
 mainframe.grid(row=0, column=0, sticky="nsew")
 mainframe.place(relx=0.5, rely=0.35, anchor="center")
 
 for col in range(MaxRow):
     for buttonRow in range(MaxButtons):
         base_font = Font(family="helvetica", size=16)
-        labelsss = Label(mainframe, font=base_font, width=4, height=2, background="black", highlightbackground="White", highlightthickness=2, bd=0, fg="white")
+        labelsss = Label(mainframe, font=base_font, width=4, height=2, background="black", highlightbackground="White", highlightthickness=2, bd=0, fg="white")  # noqa: F405
         labelsss.grid(row=col, column=buttonRow, padx=3, pady=5, sticky="nsew")
         labels.append(labelsss)
 
